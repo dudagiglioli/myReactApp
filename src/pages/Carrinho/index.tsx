fb4c7125a1e64a1d5b8a66f877aa7790ec61dee9
dudagiglioli <dudaguilice@gmail.com>
2023-08-17 08:54:29 -0300

    styles css :construction:

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import { Image, StyleSheet } from 'react-native';
-import topo from '../../../assets/topo.png'
+import { Text, Dimensions, Image, StyleSheet, View } from 'react-native';
+import topo from '../../../assets/imagens/topo.png'
+
+ //largura da tela
+const width = Dimensions.get('screen').width;
 
 export default function Carrinho() {
-    return <Image source={ topo } style={estilos.topo} />
+    return<>
+        <Image source={ topo } style={estilos.topo} />
+        <Text style= {estilos.titulo}>Detalhes do Carrinho</Text>
+        <View>
+            <Text>
+                Carrinho de Compras 
+            </Text>
+
+            <Text>
+                Fazenda Etec Cidade do Livro 
+            </Text>
+
+            <Text>
+                Uma cesta de produtos selecionados cuidadosamente da fazenda direto para a sua cozinha.
+            </Text>
+
+            <Text>
+                R$40.00
+            </Text>
+        </View>
+    </>
 }
 
+//css da imagem topo
 const estilos = StyleSheet.create({
     topo: {
-        width: "100%"
+        width: "100%",
+        height: 578 / 768 * width,
+        // altura / largura * dimensão
+    },
+
+    titulo: {
+        width: "100%",
+        position: 'absolute',
+        color:"white",
+        textAlign: "center",
+        fontSize: 24,
+        lineHeight: 26,
+        fontWeight: "bold",
+        padding: 16,
     }
 
 })
